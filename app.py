0fac8f66397fc8b1621252a4e9b913a36efe61bf
Sameen <01sameen0101@gmail.com>
2024-11-04 21:04:44 -0600

    Fixed flask app to use render port for deployment issue

--- app.py
+++ app.py
@@ -60,5 +60,6 @@ def analyze():
         'strengthColor': strength_color
     })
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
